025e836ff740e3d3ca28f9083595b66fa851c437
madiha-ansari <ansarimadiha881@gmail.com>
2025-06-21 20:44:11 +0500

    My python coading

---
+++ 1) varriable_and_datatypes/FirstProgram.py
@@ -0,0 +1,219 @@
+# introduction of python
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+# It is used for:
+
+# web development (server-side),
+# software development,
+# mathematics,
+# system scripting.
+
+
+# file name 
+# my_script.py
+#  file1.py
+# hello_world.py
+
+# string
+# print("Madiha Ansari")
+# print("Madiha Ansari")
+# print("Madiha Ansari","Madiha Ansari")
+
+# numbers
+# print(23)
+# print(23+35)
+
+# varriable
+# varriable are memory location
+# name = "madiha"  
+# age = 18
+# price = 25.99  
+# string , number ,floating value / decimals
+# print(name,age,price)
+
+# print("My name is ",name)
+# print("My age is ",age)
+
+# Mathematics
+# assign = 
+# age2 = age
+# print(age)
+# print(type(age))
+
+# rules of identify   
+# varriables name
+# allowed
+# a-z,A-Z
+# 0-9,_
+# varriable1
+
+
+# not allow 
+# %,@
+# 1varriables   
+
+# data type 5
+# interger  +,-,0,whole value ati ha
+# string    "madiha"  ,'',"",'''madiha'''
+# names = '''madiha'''
+# print(names)
+# floating value 3.99,2.5
+# boolean = True,False
+# None 
+
+# agesees = 23 ;
+# old = False;
+# a = None
+# print( type(old))
+# print(type(a))
+
+# python FirstProgram.py
+
+# python are case ssensitive languages
+# A  , a not equal
+
+# sum of two number
+# a = 5
+# b = 10
+# sum = a+b
+# print(sum)
+
+# comment 
+# """hi"""
+# print("Pakistan")
+
+
+# what is operation & operand  +operator a b operand
+# type of operator
+
+# airthmetic operator
+# a = 5
+# b = 2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) modulo   reminder
+# print(a%b)
+# print(a**b)  a^^b
+# print(a**b)
+
+# relational operator  / comparision operator
+# a = 50
+# b = 20
+# print(a==b)           false
+# print(a!=b)           true
+# print(a>=b)           true
+# print(a>b)            true
+# print(a<=b)               false
+# print(a<b)                    false
+
+# assignment operator
+# num = 10
+
+# num = num + 10  
+# num += 10   ik hi matlb ha dono ka
+
+# print(num)
+
+# sub - 
+# num = 10
+# num -= 10 
+# print(num)
+
+
+
+#mul *
+# num = 10
+# num *= 10 
+# print(num)
+
+
+# div /
+# num = 10
+# num /= 10 
+# print(num)
+
+
+
+# reminder % 
+# num = 10
+# num %= 10 
+# print(num)
+
+
+# power operator  **
+# num = 10
+# num **= 10 
+# print(num)
+
+
+# logical operator
+# (not , or , and)
+# a = 5
+# b = 10
+
+# # AND example
+# if a > 2 and b > 5:
+#     print("Both conditions are true")
+
+# # OR example
+# if a > 7 or b > 5:
+#     print("At least one condition is true")
+
+# # NOT example
+# x = True
+# print(not x)  # Output: False
+
+
+# type operator
+# airth ,real ,logic ,assi
+
+# type conversion
+# 2 type of conver 1 type conver 2 casting
+
+# a = 2 
+# b = 4.25
+# # print(a+b)   6.25
+
+# a = "2"
+# b = 4.25
+# print(a+b)   error
+
+
+# type casting
+# a = int("2")
+# b = 4.25
+# print(type(a))
+# print(a+b)      init
+
+# a = float("2")
+# b = 4.25
+# print(type(a))
+# print(a+b)     float
+
+# a = 3.12
+# a = str(a) 
+# print(type(a))   str
+
+# user sa input lana ha 
+# name = input("enter your name :")
+# print("welcome 👏",name)
+
+# val = input("enter your name : ")
+# print(type(val)," ", val)
+
+# val = int(input("enter your age : "))
+# print(type(val)," ", val)
+
+# val = float(input("enter your name : "))
+# print(type(val)," ", val)
+
+# practice question 
+name = input("Enter your name : ")
+age  = int(input("Enter your age : "))
+mark = int(input("Enter your mark: "))
+
+print("Welcome ", name)
+print("Welcome ", age)
+print("Welcome ", mark)
